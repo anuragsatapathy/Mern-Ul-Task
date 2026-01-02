7280c0999095a96ca00d37d58fbd62b1610fa093
anuragsatapathy <anurag.satapathy401@gmail.com>
2026-01-02 23:48:26 +0530

    Updated cv generaor

--- cv-generator-frontend/src/api/api.js
+++ cv-generator-frontend/src/api/api.js
@@ -4,32 +4,33 @@ const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 
 api.interceptors.response.use(
-<<<<<<< HEAD
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem("token");
-=======
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
       console.warn("Unauthorized – token invalid or expired");
 
-      //clear token
+      // clear token
       localStorage.removeItem("token");
 
-      //redirect to login
->>>>>>> c22e6c5983dbd5d9b3c8c016494601816801d841
+      // redirect to login
       window.location.href = "/login";
     }
-    return Promise.reject(err);
+
+    return Promise.reject(error);
   }
 );
 
